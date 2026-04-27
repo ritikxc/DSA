603e7427964fca30dfca88a81ef40eb35c4067dc
ritikxc <156448983+ritikxc@users.noreply.github.com>
2026-04-27 19:19:05 +0530

    Time: 3 ms (21.56%) | Memory: 9.7 MB (60.68%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+
+        if(str1 + str2 != str2 + str1) return "";
+
+        int n1 = str1.size();
+        int n2 = str2.size();
+
+        int gcd_len = gcd(n1,n2);
+
+        return str1.substr(0,gcd_len);
+    }
+};
