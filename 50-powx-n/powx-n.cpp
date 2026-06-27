6c482fbb4df8739cb4d9cb356540ca9422fde882
ritikxc <156448983+ritikxc@users.noreply.github.com>
2026-06-27 17:47:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (11.87%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    double help(double x, int n){
+
+        if(n==0) return 1.0;
+
+        double half = help(x,n/2);
+
+        if(n%2 == 0) return half*half;
+        
+        return x*half*half;
+    }
+    double myPow(double x, int n) {
+        long long N = n;
+
+        if(N<0){
+            x = 1/x;
+            N = -N;
+        }
+        return pow(x,N);
+    }
+};
