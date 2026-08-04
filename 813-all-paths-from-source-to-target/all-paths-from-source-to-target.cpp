c551c5f0a20f0f583bf0e0d16ddf4880d70c66de
ritikxc <156448983+ritikxc@users.noreply.github.com>
2026-08-04 06:08:49 +0530

    Time: 2 ms (80.50%) | Memory: 15.4 MB (33.04%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void dfs(int node, vector<vector<int>> &graph, vector<int> &path){
+        path.push_back(node);
+
+        if(node == graph.size()-1) ans.push_back(path);
+        else{
+            for(int it:graph[node]) dfs(it, graph, path);
+        }
+        path.pop_back();
+
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<int> path;
+        dfs(0, graph, path);
+        return ans;
+    }
+};
